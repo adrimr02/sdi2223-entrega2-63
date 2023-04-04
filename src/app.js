e76b69973fa74d66da76c08038419b5bffbc8e33
Adrian <adrianmartinezr2002@gmail.com>
2023-04-04 18:21:49 +0200

    Errores corregidos

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const logger = require('morgan')
 
 const app = express()
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'twig')
+
+// Middlewares setup
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -16,8 +21,8 @@ app.use(fileUpload({
   createParentPath: true
 }))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
+// Set static files
+app.use(express.static(path.join(__dirname, '../public')))
 
 
 // catch 404 and forward to error handler
