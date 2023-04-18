7445e34c049a1a8d19e87e13fe45edce623f86ab
Adrian <adrianmartinezr2002@gmail.com>
2023-04-18 20:52:23 +0200

    Terminada  funcionalidad para crear ofertas

--- src/repositories/offersRepository.js
+++ src/repositories/offersRepository.js
@@ -1,7 +1,7 @@
 module.exports = class {
   mongoClient
   app
-  collectionName = 'users'
+  collectionName = 'offers'
   /**
    * 
    * @param {import("express").Application} app 
@@ -12,6 +12,16 @@ module.exports = class {
     this.app = app
   }
 
-  
+  async insertOffer(offer) {
+    try {
+      const client = await this.mongoClient.connect(this.app.get('mongouri'))
+      const database = client.db('mywallapop')
+      const usersCollection = database.collection(this.collectionName)
+      const result = await usersCollection.insertOne(offer)
+      return result.insertedId
+    } catch (err) {
+      throw err
+    }
+  }
 
 }
--- src/routes/offers.js
+++ src/routes/offers.js
@@ -6,24 +6,43 @@
 module.exports = function(app, offerRepo) {
 
   app.get('/offers/new', (req, res) => {
-    
-
-
+    res.render('offers/new.twig')
   })
 
-  app.post('/offers/new', (req, res) => {
+  app.post('/offers/new', async (req, res) => {
     if (!req.body.title.trim() || !req.body.price.trim()) {
-      res.redirect('/offers/new?message=Debes incluir un título y un precio&messageType=alert-danger')
+      res.redirect('/offers/new?message=Debes incluir, al menos, el título y el precio&messageType=alert-danger')
       return
     }
 
     const offer = {
       title: req.body.title.trim(),
-      description: req.body.trim() || '',
-      price: parseFloat(req.body.price)
+      description: (req.body.description || '').trim(),
+      price: parseFloat(req.body.price),
+      date: new Date(),
+      author: req.session.user
     }
 
-    
+    const errors = []
+
+    if (offer.price <= 0) {
+      errors.push('El precio debe ser mayor que 0')
+    }
+
+    if (offer.title.length < 5) {
+      errors.push('El título debe tener al menos 5 caracteres')
+    }
+
+    if (offer.title.length > 30) {
+      errors.push('El título debe tener menos de 30 caracteres')
+    }
+
+    if (errors.length === 0) {
+      await offerRepo.insertOffer(offer)
+      res.redirect('offers/my-offers')
+    } else {
+      res.redirect(`/offers/new?message=${errors.join('<br>')}&messageType=alert-danger`)
+    }
 
   })
 
