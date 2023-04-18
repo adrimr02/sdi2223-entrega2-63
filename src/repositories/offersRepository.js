b0397b3622e9fe22f4049c7f08006393abd74487
Adrian <adrianmartinezr2002@gmail.com>
2023-04-18 23:23:09 +0200

    Terminada funcionalidad de listar ofertas propias y eliminar ofertas

--- src/repositories/offersRepository.js
+++ src/repositories/offersRepository.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb")
+
 module.exports = class {
   mongoClient
   app
@@ -16,9 +18,45 @@ module.exports = class {
     try {
       const client = await this.mongoClient.connect(this.app.get('mongouri'))
       const database = client.db('mywallapop')
-      const usersCollection = database.collection(this.collectionName)
-      const result = await usersCollection.insertOne(offer)
+      const offersCollection = database.collection(this.collectionName)
+      const result = await offersCollection.insertOne(offer)
       return result.insertedId
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async findOffer(filter, options) {
+    try {
+      const client = await this.mongoClient.connect(this.app.get('mongouri'))
+      const database = client.db('mywallapop')
+      const offersCollection = database.collection(this.collectionName)
+      return await offersCollection.findOne(filter, options)
+    } catch(error) {
+      throw error
+    }
+  }
+
+  async getOffersPage(filter , options, page, limit=8) {
+    try {
+      const client = await this.mongoClient.connect(this.app.get('mongouri'))
+      const database = client.db('mywallapop')
+      const offersCollection = database.collection(this.collectionName)
+      const offers = await offersCollection.find(filter, options).skip((page - 1) * limit).limit
+      (limit).toArray()
+      const totalOfferCount = await offersCollection.countDocuments()
+      return { offers, total: totalOfferCount }
+    } catch(error) {
+      throw error
+    }
+  }
+
+  async deleteOffer(id) {
+    try {
+      const client = await this.mongoClient.connect(this.app.get('mongouri'))
+      const database = client.db('mywallapop')
+      const offersCollection = database.collection(this.collectionName)
+      return await offersCollection.findOneAndDelete({ _id: new ObjectId(id) })
     } catch (err) {
       throw err
     }
--- src/routes/offers.js
+++ src/routes/offers.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb")
+
 /**
  * 
  * @param {import("express").Application} app 
@@ -6,7 +8,7 @@
 module.exports = function(app, offerRepo) {
 
   app.get('/offers/new', (req, res) => {
-    res.render('offers/new.twig')
+    res.render('offers/new')
   })
 
   app.post('/offers/new', async (req, res) => {
@@ -20,7 +22,8 @@ module.exports = function(app, offerRepo) {
       description: (req.body.description || '').trim(),
       price: parseFloat(req.body.price),
       date: new Date(),
-      author: req.session.user
+      author: req.session.user,
+      available: true
     }
 
     const errors = []
@@ -39,11 +42,85 @@ module.exports = function(app, offerRepo) {
 
     if (errors.length === 0) {
       await offerRepo.insertOffer(offer)
-      res.redirect('offers/my-offers')
+      res.redirect('/offers/my-offers?message=Oferta creada.&messageType=alert-success')
     } else {
       res.redirect(`/offers/new?message=${errors.join('<br>')}&messageType=alert-danger`)
     }
 
   })
 
+  app.get('/offers/my-offers', async (req, res) => {
+
+    const filter = { author: req.session.user }
+    const options = { sort: { date: -1 } }
+
+    const page = req.query.page || 1
+    const { offers, total: totalOfferCount } = await offerRepo.getOffersPage(filter, options, page )
+
+    const pages = getPages(totalOfferCount, page)
+
+
+    res.render('offers/myOffers', {
+      user: req.session.user,
+      offers: offers.map(o => ({ ...o, date: formatDate(o.date) })),
+      currentPage: page,
+      pages
+    })
+  })
+
+  app.get('/offers/delete/:id', async (req, res) => {
+    try {
+      const offer = await offerRepo.findOffer({ _id: new ObjectId(req.params.id) }, {})
+      if (!offer) {
+        res.redirect('/offers/my-offers?message=La oferta que intentas eliminar no existe.&messageType=alert-danger')
+        return
+      }
+      if (offer.author !== req.session.user) {
+        res.redirect('/offers/my-offers?message=Esa oferta no es tuya. No puedes eliminarla.&messageType=alert-danger')
+        return
+      }
+
+      const result = await offerRepo.deleteOffer(req.params.id)
+      if (result === null || result.deletedCount === 0) {
+        res.redirect('/offers/my-offers?message=No se ha podido eliminar la oferta.&messageType=alert-danger')
+      } else {
+        res.redirect('/offers/my-offers?message=Oferta eliminada.&messageType=alert-success')
+      }
+    } catch(err) {
+      console.log(err)
+      res.redirect('/offers/my-offers?message=Se ha producido un error al eliminar la oferta.&messageType=alert-danger')
+    }
+  })
+
+}
+
+function getPages(total, currentPage, pageSize = 8) {
+  const pages = []
+
+  let lastPage = total / pageSize
+  if (total % pageSize > 0)
+    lastPage += 1
+
+  for (let i = currentPage - 2; i <= currentPage + 2; i++)
+    if (i > 0 && i <= lastPage)
+      pages.push(i)
+
+  return pages
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns date in format DD/MM/YYYY
+ */
+function formatDate(date) {
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid "date" argument. You must pass a date instance')
+  }
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  return `${day}-${month}-${year}`
 }
